a76324f70059b0822a5eb52301a18f7eb6ba73d2
Wael <droussiwael@gmail.com>
2025-12-28 15:54:12 +0100

    Nettoyage des fichiers inutile

--- src/app/flight.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Flight {
-    id: number;
-    destination: string;
-    flightNumber: string;
-    time: string;
-    status: 'On Time' | 'Delayed' | 'Boarding' | 'Cancelled'; // On limite les choix possibles, c'est très pro
-    gate: string;
-}
